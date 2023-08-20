13b5bd62ea76cab4fcbd536921457b9fe8d56279
kfereshtanov <kfereshtanov@2create.studio>
2023-08-20 04:32:50 +0300

    Add footer styles

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(public router: Router) {}
 
+  ngOnInit(): void {}
 }
